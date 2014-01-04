82e1a27a91682c723261129dbcefe2f4c1466a6d
paolo.veronelli <paolo.veronelli@gmail.com>
2014-01-04 19:00:05 +0100

    midigui and some presets

--- Md.hs
+++ Md.hs
@@ -22,6 +22,7 @@ import qualified Data.Map as M
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
+import System.Environment
 
 -- | Loop-accept control midi message on a specific channel
 midiInDump  :: String  -- ^ client name
@@ -126,59 +127,99 @@ data Nota = Nota {
 	strength :: Int,
 	shift :: Int,
 	dur :: Int,
-	rep :: Int
+	rep :: Int,
+	space :: Int
 	} deriving (Show,Read)
 
+type Octus = M.Map Int (M.Map Int (M.Map Int Nota))
+ 
+data Track = Track {
+	volume :: Int,
+	octus :: Octus
+	}	
+
+trackAdjust f = M.adjust (\(Track v o) -> Track v $ f o)
 instance Binary Nota where
-	put (Nota p s sh d r) = put p >> put s >> put sh >> put d >> put r
+	put (Nota p s sh d r sp) = put p >> put s >> put sh >> put d >> put r >> put sp
 	get = do 
 		p <- get
 		s <- get
 		sh <- get
 		d <- get
 		r <- get 
-		return $ Nota p s sh d r
+		sp <- get 
+		return $ Nota p s sh d r sp
 
 $(deriveNFData ''Nota)
-sel 0 (Nota p s sh d r) = p
-sel 1 (Nota p s sh d r) = s
-sel 3 (Nota p s sh d r) = sh
-sel 2 (Nota p s sh d r) = d
-sel 4 (Nota p s sh d r) = r 
+
+
+instance Binary Track where
+	put (Track v o) = put v >> put o
+	get = do 
+		v <- get
+		o <- get 
+		return (Track v o)
+
+$(deriveNFData ''Track)
+
+sel 0 (Nota p s sh d r sp) = p
+sel 1 (Nota p s sh d r sp) = s
+sel 3 (Nota p s sh d r sp) = sh
+sel 2 (Nota p s sh d r sp) = d
+sel 4 (Nota p s sh d r sp) = r 
+sel 5 (Nota p s sh d r sp) = sp
+
+
 
 type Attimo = [Nota]
 
 
 
-render t ms (c,Nota p 0 sh d r) = return ()
-render t ms (c,Nota p s sh d r) = do
+render tv t ms v (c,Nota p s sh d 0 sp) = return ()
+render tv t ms v (c,Nota p 0 sh d r sp) = return ()
+render tv t ms v (c,n@(Nota p s sh d r sp)) = do
 	threadDelay $ (sh+ ms) * floor (120000*8/128)
-	forM_ [1..floor (0.5 + 128/ fromIntegral (d + 1))] $ \_ -> do 
-		atomically $ writeTChan t (c,p,s,True)
-		threadDelay $ (d + 1) * floor (0.5 + 120000*8/128)
-		atomically $ writeTChan t (c,p,0,False)
+	cv <- flip (M.!) c `fmap` atomically (readTVar tv)
+	let r = d
+	forM_ [0 .. r - 1] $ \k -> do 
+		forkIO $ do 
+			atomically $ writeTChan t (c,p,floor $ fromIntegral (r - k) / fromIntegral (r) * fromIntegral cv * fromIntegral s * fromIntegral v / 128 / 128 ,True)
+			threadDelay $ floor (120000*8/fromIntegral d * fromIntegral (127 - sp) / 128)
+			atomically $ writeTChan t (c,p,0,False)
+		threadDelay $ floor (120000*8/fromIntegral d) 
+		
 
-renderA t ms = mapM_ (\(m,x) -> forkIO . render t m $ x) . zip ms
+renderA tv t ms v = mapM_ (\(m,x) -> forkIO . render tv t m v $ x) . zip ms
 
 
 main = do
-	rem <- decodeFile "1.mde"
+	ls <- getArgs
+	let file = case ls of 
+		[] -> "last.mde" 
+		(x:_) -> x ++ ".mde"
+	rem <- decodeFile file :: IO (M.Map Int Track)
 	n <- deepseq rem `fmap` newTVarIO rem
 	(t1,t2,tn,k) <- midiInOut "paolino"
-	-- n <- newTVarIO (M.fromList . zip [0..127] . repeat $ M.fromList . zip [0..7] . repeat $ M.fromList . map (\i -> (i,Nota 0 0 0 0 1)) $ [0..127])
-	
-	trit <- newTVarIO (M.fromList . zip [0..127] . repeat $ 0)
+	-- n <- newTVarIO (M.fromList . zip [0..7] . repeat . Track 0 . M.fromList . zip [0..7] . repeat . M.fromList . zip [0..7] . repeat . M.fromList . zip [0..7] $ repeat (Nota 0 0 0 0 1 1) :: M.Map Int Track) 
+	atomically (modifyTVar n $ M.adjust (\(Track v o) -> Track 127 o) 0)
+	trit <- newTVarIO (M.fromList . zip [0..7] . repeat $ 0)
 	tedit <- newTVarIO []
+	ttrack <- newTVarIO 0
 	tsound <- newTVarIO 0
+	tvolumes <-  newTVarIO (M.fromList . zip [0..7] . repeat $ 127)
 	let pla k = do 
 		threadDelay $ 120000*8
-		forkIO (pla $ k + 1)
-		f <- atomically $ do 
-			ms <- readTVar trit
-			z <- readTVar n
-			return $ \f -> f ms z 
-		f $ \ms z  ->  forM_ [0..7] $ \r -> renderA tn (M.elems $ ms) . (M.assocs . flip (M.!) r . flip (M.!) ((k `mod` 8))) $ z
-	forkIO $ pla 0
+		forkIO $ do 
+			f <- atomically $ do 
+				ms <- readTVar trit
+				zt <- readTVar n
+				return $ \f -> f ms zt
+			f $ \ms zt  ->  forM_ [0..7] $ \t -> do
+				let Track v z = zt M.! t
+				forM_ [0..7] $ 
+					\r -> renderA tvolumes tn (M.elems $ ms) v . (M.assocs . flip (M.!) r . flip (M.!) ((k `mod` 8))) $ z
+		pla $ k + 1
+	w <- forkIO $ pla 0
 	forkIO $ forever $ do 
 		 (c_,p_,v_) <- atomically $ readTChan t1
 		 print (c_,p_,v_);
@@ -186,62 +227,91 @@ main = do
 				126 -> do
 					t <- zonedTimeToLocalTime `fmap` getZonedTime
 					z <- atomically (readTVar n) 
-					writeFile (show t) . show $ z
-				-- sound delay
+					encodeFile (show t ++ ".mde") $ z
+				-- view a piece
 				118 -> atomically $ do
-					z <- readTVar n	
+					t <- readTVar ttrack
+					z <- octus `fmap` flip (M.!) t `fmap` readTVar n	
 					c_ <- readTVar tsound
-					mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! v_) M.! i) M.! c_)) | j <- [0..4] , i <- [0..7]]
-
+					mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! v_) M.! i) M.! c_)) | j <- [0..5] , i <- [0..7]]
+				-- sound delay
 				125 -> atomically $ do 
 						c_ <- readTVar tsound
 						modifyTVar trit . flip M.adjust c_ $ \_ -> v_
-				123 -> atomically $ do 
+				-- change sound
+				123 -> atomically $ when (v_ < 8) $ do 
 						writeTVar tsound v_
 					 	mb <- readTVar tedit
 						c_ <- readTVar tsound
 						case mb of 
 							[] -> return ()
 							(bank:_) -> do
-								z <- readTVar n	
-								mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! bank) M.! i) M.! c_)) | j <- [0..4] , i <- [0..7]]
+								t <- readTVar ttrack
+								z <- octus `fmap` flip (M.!) t `fmap` readTVar n	
+								mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! bank) M.! i) M.! c_)) | j <- [0..5] , i <- [0..7]]
+							
+				119 -> atomically $ do 
+					writeTVar ttrack v_
+					mb <- readTVar tedit
+					c_ <- readTVar tsound
+					case mb of 
+						[] -> return ()
+						(bank:_) -> do
+							t <- readTVar ttrack
+							z <- octus `fmap` flip (M.!) t `fmap` readTVar n	
+							mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! bank) M.! i) M.! c_)) | j <- [0..5] , i <- [0..7]]
+
 				-- change value
-				_ -> if p_ `elem` [110 .. 117] then let p = p_ - 110 in 
-					case v_ of 
-						0 -> atomically $ do 
-							modifyTVar tedit $ delete p
-							c_ <- readTVar tsound
+				_ -> 	if p_ `elem` [102 .. 109] then let p = p_ - 102 in
+						atomically $ modifyTVar tvolumes $ flip M.adjust p $ const v_
+				    	else if p_ `elem` [110 .. 117] then let p = p_ - 110 in 
+						case v_ of 
+							0 -> atomically $ do 
+								modifyTVar tedit $ delete p
+								c_ <- readTVar tsound
+								mb <- readTVar tedit
+								case mb of 
+									[] -> return ()
+									(bank:_) -> do
+										t <- readTVar ttrack
+										z <- octus `fmap` flip (M.!) t `fmap` readTVar n	
+										mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! bank) M.! i) M.! c_)) | j <- [0..5] , i <- [0..7]]
+							_ -> atomically $ do 
+								modifyTVar tedit $ (p :)
+								c_ <- readTVar tsound
+								t <- readTVar ttrack
+								z <- octus `fmap` flip (M.!) t `fmap` readTVar n	
+								mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! p) M.! i) M.! c_)) | j <- [0..5] , i <- [0..7]]
+
+						
+
+						else atomically $ do 
 							mb <- readTVar tedit
+							c_ <- readTVar tsound
+							t <- readTVar ttrack
 							case mb of 
 								[] -> return ()
-								(bank:_) -> do
-									z <- readTVar n	
-									mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! bank) M.! i) M.! c_)) | j <- [0..4] , i <- [0..7]]
-						_ -> atomically $ do 
-							modifyTVar tedit $ (p :)
-							c_ <- readTVar tsound
-							z <- readTVar n	
-							mapM_ (writeTChan t2) [(0,j*8 + i,sel j $ (((z M.! p) M.! i) M.! c_)) | j <- [0..4] , i <- [0..7]]
-
-					
-
-					else atomically $ do 
-						mb <- readTVar tedit
-						c_ <- readTVar tsound
-						case mb of 
-							[] -> return ()
-							banks -> forM_ banks $ \bank -> 
-								modifyTVar n . flip M.adjust bank . flip M.adjust (p_ `mod` 8) . flip M.adjust c_ $ 
-									\(Nota p s sh d r) -> case (p_ `div` 8) of 
-										0 -> (Nota v_ s sh d r)
-										1 -> (Nota p v_ sh d r)
-										2 -> (Nota p s sh v_ r)
-										3 -> (Nota p s v_ d r)
-										4 -> (Nota p s sh d v_)
-										_ -> (Nota p s sh d r)
-						 
+								banks -> forM_ banks $ \bank -> 
+									modifyTVar n . flip trackAdjust t .   
+											flip M.adjust bank . 
+											 flip M.adjust (p_ `mod` 8) . 
+											  flip M.adjust c_ $ 
+										\(Nota p s sh d r sp) -> case (p_ `div` 8) of 
+											0 -> (Nota v_ s sh d r sp)
+											1 -> (Nota p v_ sh d r sp)
+											2 -> (Nota p s sh v_ r sp)
+											3 -> (Nota p s v_ d r sp)
+											4 -> (Nota p s sh d v_ sp)
+											5 -> (Nota p s sh d r v_)
+											_ -> (Nota p s sh d r sp)
+							 
 	l <- getLine
+	killThread w
+	threadDelay $ 120000*16
 	z <- atomically $ readTVar n
-	encodeFile "1.mde" z
+	case l of
+		[] -> encodeFile "last.mde" z
+		file -> encodeFile (file ++ ".mde") z
+		
 		 
 		
