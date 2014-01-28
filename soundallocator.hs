694aad141654e9faddda0a99ec013f7cd61748ba
paolo.veronelli <paolo.veronelli@gmail.com>
2014-01-28 15:37:40 +0100

    nice one 1048 is not 1024

--- soundallocator.hs
+++ soundallocator.hs
@@ -43,7 +43,7 @@ bootSample :: Int -> (Int,FilePath) -> IO ()
 bootSample j (n,fp) = do
         withSC3n j . send $ b_free n
         withSC3n j . send $ b_allocRead n fp 0 0
-        withSC3n j . send $ d_recv . synthdef (show n) . out 0 $ envGen KR 1 1 0 1 RemoveSynth (envPerc 0.001 (control KR "rel" 0.1)) *  (control KR "amp" 0) * orig
+        withSC3n j . send $ d_recv . synthdef (show n) . out 0 $ envGen KR 1 1 0 1 RemoveSynth (envPerc' 0.001 (control KR "rel" 0.1) 1.0 (EnvNum 1, EnvNum 1)) *  (control KR "amp" 0) * orig
         where orig =  playBuf 2 AR (fromIntegral n) (0.5 + 1.5 * control KR "pitch" 0) 1 0 NoLoop RemoveSynth  
 
 
@@ -62,14 +62,14 @@ initSynths globs = do
                 l@(i,_) <- zip [0..] ls
                 return $  do 	
 			bootSample j l
-			-- playSample i (t + 3 + fromIntegral i * 0.005) 0.5 0.1 (0.005)
+			playSample i (t + 3 + fromIntegral i * 0.005) 0.5 0.1 (0.005)
 	
 	return ls
 
 newtype Sequencer = Sequencer ((Time -> Double -> IO ()) -> IO Sequencer)
 
 quantize t0 dt d't = t0 + (fromIntegral (floor $ dt/d't)  + 1) * d't
-sequencer :: Time -> Time -> TVar Integer -> TVar Integer -> IO (Sequencer,TVar Integer)
+sequencer :: Time -> Time -> TVar Integer -> TVar Integer -> IO (Sequencer,TVar Integer,TVar Double)
 sequencer ot t0 tp td  = do
 	tz <- newTVarIO 34
 	tq <- newTVarIO 1
@@ -81,39 +81,40 @@ sequencer ot t0 tp td  = do
 			p <- readTVar tp
 			d <- readTVar td
 			let 	
-				(n,z) = (d*v +  p) `divMod` 256
+				(n,z) = (d*v +  p) `divMod` 128
 				
 				-- v'= z
-				v' = (head $ dropWhile (<=z) $ iterate (*2) 1)
-				d't = fromIntegral v'/1048
+				z' = if z < 15 then 15 else z
+				v' = (head $ dropWhile (<=z') $ iterate (*2) 1)
+				d't = fromIntegral v'/1024
 				timev' = fromIntegral n * d't + quantize t0 dt d't
 				nexttick = quantize t0 dt q
-				(tev,val) = if nexttick > timev' then (timev',z) else (nexttick,v0)
+				(tev,val) = if nexttick > timev' + 1/64 then (timev',z) else (nexttick,v0)
 				
 			return $ trace (show (tev,val)) $  (tev, val)
-		f  t' (fromIntegral v'/ 256)
+		f  t' (fromIntegral v'/ 128)
 		pauseThreadUntil t'
 		return (Sequencer (g v' t'))
-	return (Sequencer (g 34 t0), tz) 
+	return (Sequencer (g 34 t0), tz, tq) 
 
-samplesequencer :: Time -> Time -> Integer -> IO (TVar Integer,TVar Integer, TVar Integer, TVar Integer,TVar Double,TVar Integer)
+samplesequencer :: Time -> Time -> Integer -> IO (TVar Integer,TVar Integer, TVar Integer, TVar Integer,TVar Double,TVar Integer,TVar Double)
 samplesequencer ot t0 d = do
 	tp <- newTVarIO 43
 	td <- newTVarIO d
 	tv <- newTVarIO 0
 	tf <- newTVarIO 0
 	tsa <- newTVarIO 0
-	(s,tz) <- sequencer ot t0 tp td  
+	(s,tz,tq) <- sequencer ot t0 tp td  
 	let loop n (Sequencer f)  = do
 		f' <- atomically $ do 
 			d <- readTVar td
 			v <- readTVar tv
 			fh <- readTVar tf
 			sa <- readTVar tsa
-			return $ \t r -> playSample sa (t + fromIntegral fh / 128 * 0.5 ) 0.5 (r*v) (4*r)
+			return $ \t r -> playSample sa (t + 0.2) 0.5 (r*v) (4*r)
 		f f' >>= loop (n + 1)
 	forkIO $ loop 0 s
-	return (tf,tsa,tp,td,tv,tz)
+	return (tf,tsa,tp,td,tv,tz,tq)
 unzip4 = foldr f ([],[],[],[]) where
 		f (x,y,z,g) (xs,ys,zs,gs) = (x:xs,y:ys,z:zs,g:gs)
 unzip5 = foldr f ([],[],[],[],[]) where
@@ -125,7 +126,7 @@ unzip7 = foldr f ([],[],[],[],[],[],[]) where
 main = do 
 	ls <- initSynths "/home/paolino/WAV/*/*/*.wav"
 	t <- time
-	(tf,tsa,tp,td,tv,tz) <- unzip6 `fmap` (replicateM 8 $ samplesequencer 1 t 64)
+	(tf,tsa,tp,td,tv,tz,tq) <- unzip7 `fmap` (replicateM 8 $ samplesequencer 1 t 64)
 	mc <- newTChanIO
 	forkIO $ midiIn "samples" 0 mc
 	forever $ do 
@@ -137,7 +138,7 @@ main = do
 					1 -> modifyTVar (tp !! y) . const $ fromIntegral s
 					2 -> modifyTVar (td !! y) . const $ fromIntegral s 
 					3 -> modifyTVar (tv !! y) . const $ fromIntegral s / 128
-					4 -> modifyTVar (tf !! y) . const $ fromIntegral s 
+					4 -> modifyTVar (tq !! y) . const $ (fromIntegral s + 1) / 16
 					5 -> modifyTVar (tz !! y) . const $ fromIntegral s 
 					_ -> return ()
 			
